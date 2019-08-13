68ed675b0c74e197b2500ba01f1c806f45e36e59
Velliangiri <mohanprabu.velliangiri@bms.com>
2019-08-13 19:24:47 +0530

    added avatar

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 from app import db, login
+
 
 @login.user_loader
 def load_user(id):
@@ -22,6 +24,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
